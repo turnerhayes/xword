4c3b1156af06c3240eeb49ff44a43a2c08d34103
Turner Hayes <dev@turnerhayes.com>
2016-01-18 15:27:39 -0500

    Initial code commit

---
+++ app.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var express      = require('express');
+var path         = require('path');
+var fs           = require('fs');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var hbs          = require('express-hbs');
+
+var config = require('./lib/utils/config');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('hbs', hbs.express4({
+	defaultLayout: path.join(config.paths.templates, 'layout.hbs'),
+	partialsDir: config.paths.partials,
+}));
+
+app.set('views', config.paths.templates);
+app.set('view engine', 'hbs');
+
+app.set('env', config.app.environment);
+app.locals.IS_DEVELOPMENT = config.app.environment === 'development';
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/static', express.static(config.paths.static));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+app.use(function(err, req, res, next) {
+	var status = err.status || 500;
+
+	var errorTemplateName = path.join('errors', '' + status);
+	var errorTemplatePath = path.join(config.paths.templates, errorTemplateName + '.hbs');
+
+	res.status(status);
+
+	fs.stat(
+		errorTemplatePath,
+		function(statError) {
+			if (statError && statError.code === 'ENOENT') {
+				errorTemplateName = 'errors/500';
+			}
+
+			res.render(errorTemplateName, {
+				req: req,
+				message: err.message,
+				// no stacktraces leaked to user in production
+				error: config.app.environment === 'development' ?
+					err :
+					{}
+			});
+		}
+	);
+});
+
+module.exports = app;
---
+++ static/scripts/init/handlebars.es6
@@ -0,0 +1,15 @@
+import _          from "lodash";
+import Handlebars from "hbsfy/runtime";
+import getHelpers from "../../../hbs-helpers";
+import partials   from "../partials";
+
+export default function init() {
+	Handlebars.registerHelper(getHelpers(Handlebars));
+
+	_.each(
+		partials,
+		function(compiledPartial, partialName) {
+			Handlebars.registerPartial(partialName, compiledPartial);
+		}
+	);
+}
