5de6e210cbfb55fbeb2aec0b8ca18a4ff6322223
Turner Hayes <dev@turnerhayes.com>
2016-02-26 21:07:26 -0500

    Added data store config for Heroku setup

--- heroku-setup.js
+++ heroku-setup.js
@@ -3,3 +3,4 @@
 var debug = require('debug')('xword:heroku-setup');
 
 process.env.CONFIG_SESSION_STORE_URL = process.env.MONGOLAB_URI;
+process.env.CONFIG_DATA_STORE_URL = process.env.MONGOLAB_URI;
